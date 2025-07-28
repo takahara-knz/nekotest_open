e786801286061e10c8238e639c8e9b94f1d3a7a4
Neko Takahara <111856834+takahara-knz@users.noreply.github.com>
2025-07-28 16:48:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="ふかふかテスト", layout="centered")
 st.title("ふかふかStreamlit Cloudテスト")
 st.write("この画面が見えたら、Cloudはちゃんと動いてるよ！")
+st.success("ふかふか成功してほしいなぁ…！")
